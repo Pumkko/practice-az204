c98bacd44cf0f384d245444b3045b9afd9fdb61e
pumkko <c.coque@hotmail.fr>
2024-02-19 15:53:37 -0500

    add app insight telemetry

--- BikeRepairAppointmentApi/BikeRepairAppointmentApi/Program.cs
+++ BikeRepairAppointmentApi/BikeRepairAppointmentApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationInsightsTelemetry();
+
 
 builder.Services.AddCors(options =>
 {
